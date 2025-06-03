8fb6604abe65108a9392eee5517bb099733d3e12
pankeki <barbara.tereze@gmail.com>
2025-06-03 11:54:36 +0300

    login pievienot

--- temporary_name/temporary_name/settings.py
+++ temporary_name/temporary_name/settings.py
@@ -127,9 +127,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# If you're using a top-level static folder:
+import os
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, "static")
 ]
 
 # Default primary key field type
--- temporary_name/temporary_name/urls.py
+++ temporary_name/temporary_name/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from projects import views as project_views
+from .views import login_view
 
 urlpatterns = [
+    path('login/', login_view, name='login'),
+    path('', include('login.urls')),
     path('admin/', admin.site.urls),
     path('', project_views.index, name='home'),  # root URL izsauc index skatu
     path('projects/', include('projects.urls')),
